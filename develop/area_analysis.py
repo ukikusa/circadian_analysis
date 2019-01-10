a4f94438b641150e0489f8ca340576388741b66a
kenya <ueno.kenya.32c@st.kyoto-u.ac.jp>
2019-01-10 09:45:05 +0900

    面積の増殖率について，横軸をNにした出力に

--- develop/area_analysis.py
+++ develop/area_analysis.py
@@ -29,7 +29,7 @@ def per_dif(x, data_v, dif_n):
     return x, per_dif
 
 
-def plot_dif(x, y, save_path, title=False, ylabel='increace_rate(%)', log=False, label=False):
+def plot_dif(x, y, save_path, title=False, ylabel='increace_rate(%)', log=False, label=False, use_day=8):
     fig = plt.figure(figsize=(6, 4), dpi=100)
     time, data_n = y.shape
     for i in range(data_n):
@@ -37,12 +37,34 @@ def plot_dif(x, y, save_path, title=False, ylabel='increace_rate(%)', log=False,
     if log is True:
         plt.yscale("log")
         # plt.ylim(bottom=0.01, top=100)
-    plt.xlim(0, 24 * 8)
-    plt.xticks(range(0, 24 * 8, 24))
-    plt.xlabel('time(h)')
+    plt.xlim(0, 24 * use_day)
+    plt.xticks(range(0, 24 * use_day, 24))
+    plt.xlabel("time(h)")
     plt.ylabel(ylabel)
     plt.grid(which='major', color='black', linestyle=':')
     if ylabel == 'increace_rate(%)' and log is False:
+        plt.ylim(bottom=-0.5)
+    if label is not False:
+        plt.legend(label)
+    if title is not False:
+        plt.title(title, fontsize=6)
+    plt.savefig(save_path, format='pdf')
+    plt.close()
+
+
+def plot_N_dif(x, y, save_path, title=False, log=False, label=False):
+    fig = plt.figure(figsize=(6, 4), dpi=100)
+    time, data_n = y.shape
+    for i in range(data_n):
+        plt.plot(x[:, i], y[:, i], lw=0, color=cm.jet(i / data_n), marker=".")
+    if log is True:
+        plt.yscale("log")
+        # plt.ylim(bottom=0.01, top=100)
+    plt.xlim(left=0)
+    plt.xlabel("N")
+    plt.ylabel("increace_rate(%)")
+    plt.grid(which='major', color='black', linestyle=':')
+    if log is False:
         plt.ylim(bottom=-0.5)
     if label is not False:
         plt.legend(label)
@@ -61,7 +83,7 @@ def align_nan(data):
     return data_new
 
 
-def area_analysis(data, save, dt=60, avg=False, label=False):
+def area_analysis(data, save, dt=60, avg=False, label=False, use_day=8):
     data_v = data.values
     data_v[data_v == 0] = np.nan
     x = list(data.index)
@@ -75,15 +97,14 @@ def area_analysis(data, save, dt=60, avg=False, label=False):
     if os.path.exists(folder) is False:
         os.makedirs(folder)
     pp = PdfPages(save)
-    plot_dif(x, data_v, save_path=pp, title='averate_' + str(avg) + '-diff_24', ylabel='area(pixel)', label=label)
-    plot_dif(x, data_v, save_path=pp, title='averate_' + str(avg) + '-diff_24', ylabel='area(pixel)', label=label, log=True)
-    plot_dif(x_dif, data_dif, save_path=pp, title='average_-diff_24', ylabel='diff(pixel)', label=label)
-    plot_dif(x24, per_dif24, save_path=pp, title='average_' + str(avg) + '-diff_24', label=label)
-    plot_dif(x24, per_dif24, save_path=pp, title='average_' + str(avg) + '-diff_24', log=True, label=label)
+    plot_dif(x, data_v, save_path=pp, title='averate_' + str(avg) + '-diff_24', ylabel='area(pixel)', label=label, use_day=use_day)
+    plot_dif(x, data_v, save_path=pp, title='averate_' + str(avg) + '-diff_24', ylabel='area(pixel)', label=label, log=True, use_day=use_day)
+    plot_dif(x_dif, data_dif, save_path=pp, title='average_-diff_24', ylabel='diff(pixel)', label=label, use_day=use_day)
+    plot_N_dif(data_v[:-dif_n], per_dif24, save_path=pp, title='average_' + str(avg) + '-diff_24', label=label)
+    plot_N_dif(data_v[:-dif_n], per_dif24, save_path=pp, title='average_' + str(avg) + '-diff_24', log=True, label=label)
     ###############################
     # 揃えた方
     ###########################
-    per_dif24_align = align_nan(per_dif24)
-    plot_dif(x24, per_dif24_align, save_path=pp, title='align_average_' + str(avg) + '-diff_24', label=label)
-    plot_dif(x24, per_dif24_align, save_path=pp, title='align_average_average_' + str(avg) + '-diff_24', log=True, label=label)
+    data_v_align = align_nan(data_v)
+    plot_dif(x, data_v_align, save_path=pp, title='align_average_' + str(avg), ylabel='area(pixel)', label=label, use_day=use_day)
     pp.close()
